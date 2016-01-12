452d8d859425a74992b61f4fd0e0f64d3f70aa91
akash13singh <aakashsingh@gmail.com>
2016-01-12 20:22:41 +0100

    Fix for PUT request form bindingw

--- js/app.js
+++ js/app.js
@@ -1,48 +1,32 @@
-(function(){
-	var app = angular.module('reservation',[]);
-	
+(function() {
+	var app = angular.module('reservation', []);
+
 	app.controller("ParkingSpotsCtrl", function($scope, $http) {
-		  /*$http.get('http://localhost:8080/api/clients').
-		    success(function(data, status, headers, config) {
-		      $scope.spots = data;
-		    }).
-		    error(function(data, status, headers, config) {
-		      // log error
-		    });*/
-		  
-		  this.getParkingInfo = function(){
-			  
-			  $http.get('http://localhost:8080/api/reservation').
-			    success(function(data, status, headers, config) {
-			      $scope.parkingLots = data;
-			    }).
-			    error(function(data, status, headers, config) {
-			      // log error
-			    });
-		    };
+
+		this.getParkingInfo = function() {
+
+			$http.get('http://localhost:8080/api/reservation').success(
+					function(data, status, headers, config) {
+						$scope.parkingLots = data;
+					}).error(function(data, status, headers, config) {
+				// log error
+			});
+		};
 
 	});
-	
+
 	app.controller("ReservationCtrl", function($scope, $http) {
-		$scope.reserveParking = function(){
-			 
-			 var data = {
-				 parkingClientId: "p123",
-	             parkingSpotId: "s123",
-	             vehicleId: "v123"
-	            };
-				window.alert(data+"fff");  
-		 $http.put('http://localhost:8080/api/reservation',data)
-         .success(function (data, status, headers) {
-             $scope.ServerResponse = "spot reserved";
-         })
-         .error(function (data, status, header, config) {
-             $scope.ServerResponse =  htmlDecode("Data: " + data +
-                 "\n\n\n\nstatus: " + status +
-                 "\n\n\n\nheaders: " + header +
-                 "\n\n\n\nconfig: " + config);
-         });  
+		$scope.reserveParking = function() {
+			console.log($scope.reservation);
+			$http.put('http://localhost:8080/api/reservation',
+					$scope.reservation).success(
+					function(data, status, headers) {
+						$scope.ServerResponse = "spot reserved";
+					}).error(
+					function(data, status, header, config) {
+						//log error
+					});
 		};
-	});	
-		  
+	});
+
 })();
