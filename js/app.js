ba518bc2eec2d48c178d90ef944ff0ce4079d70e
akash13singh <aakashsingh@gmail.com>
2016-01-12 22:10:34 +0100

    Corrected Put url

--- js/app.js
+++ js/app.js
@@ -18,8 +18,11 @@
 	app.controller("ReservationCtrl", function($scope, $http) {
 		$scope.reserveParking = function() {
 			console.log($scope.reservation);
-			$http.put('http://localhost:8080/api/reservation',
-					$scope.reservation).success(
+			console.log($scope.reservation.parkingClientId);
+			params = {"id":32802,"value":$scope.reservation.vehicleId,"action":"reserve"}
+			// http://localhost:8080/api/reservation/0b6b66cb-4c4d-4b9a-ac29-e20b5029b408/32700/0/32802  {"id":32802,"value":"vehicle325","action":"reserve"}
+			$http.put('http://localhost:8080/api/reservation/'+$scope.reservation.parkingClientId+'/32700/0/32802',
+					params).success(
 					function(data, status, headers) {
 						$scope.ServerResponse = "spot reserved";
 					}).error(
